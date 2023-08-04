f38c8d6b00d865295ed89bb7ac2ce5ccd480619c
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-08-04 16:10:21 +0300

    Initial Commit

---
+++ NyNameTestd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NyNameTestd
+//
+//  Created by Валерия Дементьева on 04.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
